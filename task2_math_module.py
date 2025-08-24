ca3d540875bcd4264fcfe8fd3edb1f5d64decfc2
Anqwert01 <Anqwert01@proton.me>
2025-08-24 19:32:04 +0530

    Add files via upload

---
+++ task2_math_module.py
@@ -0,0 +1,11 @@
+import math
+
+num = int(input("Enter a number: "))
+
+square_root = math.sqrt(num)
+logarithm = math.log(num)
+sine_value = math.sin(num)
+
+print(f"Square root: {square_root}")
+print(f"Logarithm: {logarithm}")
+print(f"Sine: {sine_value}")
